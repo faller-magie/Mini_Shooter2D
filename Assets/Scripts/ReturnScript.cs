8d74ab20117e6c1bbe94976c35014d2338ed13ad
Fab <fallermagie@yahoo.fr>
2020-06-18 11:59:47 +0200

    Adding Prefabs & Return Button Changes
    -Ajout du dossier "Prefabs" (très utile pour réutiliser mon bouton return dans plusieurs scènes)
    -Création d'un bouton UI en préfab et reutilisé dans 2 scènes : Instructions et Crédits

---
+++ Assets/Scripts/ReturnScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnScript : MonoBehaviour
+{
+    //Mon bouton "Retour" que je vais utiliser en prefab.
+    public void returnButton()
+    {
+        SceneManager.LoadScene("MainMenu"); //Chargement de la scène Main Menu
+    }
+}
