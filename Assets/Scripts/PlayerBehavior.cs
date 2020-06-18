cdb686f86ca269797de934a48eaac431c985a7ca
Fab <fallermagie@yahoo.fr>
2020-06-18 16:00:39 +0200

    Setup Tilemap + PlayerBehavior Changes
    Préparation des tilemaps
    Création d'un rule tile
    Ajout d'un tilemap dans la hierarchy
    Création d'un début de niveau
    Modifications sur le script PlayerBehavior

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,41 +7,45 @@ public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private GameObject bullet;
 
     private Shooter inputs;
     private Rigidbody2D corps;
     private Vector2 direction;
 
 
-    private void OnEnable()
+    // Start is called before the first frame update
+    void Start()
     {
         inputs = new Shooter();
         inputs.Enable();
         inputs.Player.Move.performed += OnMovePerfomed;
         inputs.Player.Move.canceled += OnMoveCanceled;
+        inputs.Player.Shoot.performed += OnShootPerformed;
+    }
+    //La physique de deplacement du joueur, géré par unity
+     private void FixedUpdate()
+    {
+        corps.velocity = direction * (speed * Time.fixedDeltaTime);
     }
 
+    //Quand le joueur appuye sur la touche de deplacement
     private void OnMovePerfomed(InputAction.CallbackContext obj)
     {
         Debug.Log("Move en marche");
         direction = obj.ReadValue<Vector2>();
     }
 
+    //Quand le joueur n'appuie PAS sur la touche de deplacement
     private void OnMoveCanceled(InputAction.CallbackContext obj)
     {
         Debug.Log("Statue");
-        direction = Vector2.zero;
+        direction = Vector2.zero; //notre variable à pour valeur 0 donc ça bouge pas..
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnShootPerformed(InputAction.CallbackContext obj)
     {
-        
-    }
-
-    // Update is called once per frame
-    private void FixedUpdate()
-    {
-        
+        // On instantie un projectile à la position du joueur avec une rotation nulle
+        Instantiate(bullet,transform.position, Quaternion.identity);
     }
 }
