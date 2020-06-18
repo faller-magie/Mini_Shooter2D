6f48646b433ef44000756eb6f19e5ffa86bd4582
Fab <fallermagie@yahoo.fr>
2020-06-18 11:49:37 +0200

    Adding ReturnScript + Changes Menu
    -Ajout du script ReturnScript
    -Modifs sur le menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,25 +6,25 @@ using UnityEngine.SceneManagement; //Ajout du scene management car j'ai besoin d
 public class MenuScript : MonoBehaviour
 {
     //Mon bouton "Jouer"
-    private void playButton()
+    public void playButton()
     {
         SceneManager.LoadScene("Game"); //Chargement de la scène "Game"
     }
 
     //Mon bouton "Instructions"
-    private void howToButton()
+    public void howToButton()
     {
         SceneManager.LoadScene("Instructions");//Chargement de la scène "Instructions"
     }
 
     //Mon bouton "Crédits"
-    private void creditsButton()
+    public void creditsButton()
     {
         SceneManager.LoadScene("Credits");//Chargement de la scène "Crédits"
     }
 
     //Mon bouton "Quitter"
-    private void quitButton()
+    public void quitButton()
     {
         Debug.Log("Quitte le jeu");
         Application.Quit(); //Arrête l'application et ferme le jeu
